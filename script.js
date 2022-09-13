a866875159c79583f8818994c638882466076fb2
Stalloyde <stalloyde93@gmail.com>
2022-09-13 23:09:15 +0800

    Complete skeletal layout

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 const form = document.querySelector(".form");
+const close = document.querySelector(".close");
 const addBook = document.querySelector(".add");
-addBook.addEventListener("click", function () {form.style.display = "flex"})
+addBook.addEventListener("click", function () {form.style.display = "flex"});
+close.addEventListener("click", function () {form.style.display = "none"});
