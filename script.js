003b7379e9c03b11b926401a75f99182d4a812e4
Stalloyde <stalloyde93@gmail.com>
2022-09-14 10:38:37 +0800

    Add constructor, invocation and push to book array

--- script.js
+++ script.js
@@ -1,5 +1,22 @@
 const form = document.querySelector(".form");
 const close = document.querySelector(".close");
-const addBook = document.querySelector(".add");
-addBook.addEventListener("click", function () {form.style.display = "flex"});
+const openAddForm = document.querySelector(".add");
+openAddForm.addEventListener("click", function () {form.style.display = "flex"});
 close.addEventListener("click", function () {form.style.display = "none"});
+
+const books = [];
+
+function Book (title, author, pages, status) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.status = status;
+}
+
+function addBook (title,author,pages,status) {
+    const newBook = new Book(title, author,pages,status);
+    books.push(newBook);
+}
+
+addBook('asd','qwe',123,'not read');
+console.log(books)
