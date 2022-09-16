998b4fda3d2df2fca9d2ef196b5b90452b3b1042
Stalloyde <stalloyde93@gmail.com>
2022-09-16 15:02:07 +0800

    Add and style edit & delete action buttons

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const form = document.querySelector(".form");
 const close = document.querySelector(".close");
-const openAddForm = document.querySelector(".add");
-const addBookButton = document.querySelector(".add");
+const openAddForm = document.querySelector(".addBookButton");
 const tableDiv = document.querySelector(".table");
 openAddForm.addEventListener("click", function () {form.style.display = "flex"});
 close.addEventListener("click", function () {form.style.display = "none"});
@@ -25,28 +24,34 @@ addBook('first book','author',123,'read');
 addBook('second book','author',51341,'not read');
 addBook('Third book','tester',45462,'read');
 
-function displayTable () {
-    const table = document.createElement("table");
-    let tr = document.createElement("tr")
-    const header = ['title', 'author', 'pages', 'status'];
-    
-    for (let i=0; i< header.length; i++) {
-        let th = document.createElement("th");
-        th.textContent = header[i].charAt(0).toUpperCase() + header[i].slice(1);
-        tr.appendChild(th);
-        table.appendChild(tr);
-    }   
+const table = document.createElement("table");
+let tr = document.createElement("tr")
+const header = ['title', 'author', 'pages', 'status'];
 
-    for (let x=0; x<books.length; x++) { 
-        let tr = document.createElement("tr");
-        
-        for (const y in books[x]) {
-            let td = document.createElement("td");
-            td.textContent = books[x][y]; 
-            tr.appendChild(td);
-            table.appendChild(tr);
-        }      
-    tableDiv.appendChild(table);
-    }
+for (let i=0; i< header.length; i++) {
+    let th = document.createElement("th");
+    th.textContent = header[i].charAt(0).toUpperCase() + header[i].slice(1);
+    tr.appendChild(th);
+    table.appendChild(tr);
+}   
+
+for (let x=0; x<books.length; x++) { 
+    let tr = document.createElement("tr");
+
+    for (const y in books[x]) {
+        let td = document.createElement("td");  
+        td.textContent = books[x][y]; 
+        tr.appendChild(td);
+        table.appendChild(tr);
+    }      
+
+const edit = document.createElement("button");
+const del = document.createElement("button");
+edit.classList.add("action-button","edit-button");
+del.classList.add("action-button","del-button");
+edit.textContent = "Edit";
+del.textContent = "Delete";
+tr.appendChild(edit);
+tr.appendChild(del);
+tableDiv.appendChild(table);
 }
-displayTable()
